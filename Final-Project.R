695b7337fdb47f254308b7754a7843a919d01f6e
federicovicentini <federico.vicentini99@gmail.com>
2022-07-11 17:43:58 +0200

    Changes

--- Final-Project.R
+++ Final-Project.R
@@ -27,3 +27,61 @@ invisible(lapply(packages, library, character.only = TRUE))
 # Load packages
 library(quantmod)
 library(eurostat)
+
+
+
+#Import data
+# Download inflation rates from Fred
+freddata <- c("PCEPI")
+for (i in 1:length(freddata)) {
+  getSymbols(freddata[i], src = "FRED")
+}
+plot(PCEPI)
+
+#Check for NA values
+nas=c(which(is.na(PCEPI)))
+nas
+
+#Check for null values
+zeroes=c(which(PCEPI==0))
+zeroes
+
+#Transform PCEPI in PCE inflation rates
+#which is a timeseries with monthly yoy
+#inflation rates
+
+inflationus=c()
+for(i in 13:length(PCEPI)){
+  inflationus[i]=((as.numeric(PCEPI[i])-as.numeric(PCEPI[i-12]))/(as.numeric(PCEPI[i-12])))*100
+}
+plot(inflationus, type="l")
+for(i in 13:length(PCEPI)){
+  PCEPI[i]=inflationus[i]
+}
+PCEPI[1:12]=NA
+PCEPI=na.omit(PCEPI)
+
+
+plot(PCEPI, type="l")
+
+
+#Store Canada, Norway, Sweden, and UK CPI data
+canadacpi=read.csv("CanadaCPI2.csv", sep=",")
+canadacpi <- canadacpi %>%
+  filter(Products.and.product.groups == "All-items") %>%
+  filter(GEO == "Canada")
+time=canadacpi[,1]
+cutcanadacpi=as.data.frame(canadacpi[,11])
+cutcanadacpi[,2]=canadacpi[,1]
+cutcanadacpi[,3]=cutcanadacpi[,2]
+cutcanadacpi[,2]=cutcanadacpi[,1]
+cutcanadacpi[,1]=cutcanadacpi[,3]
+cutcanadacpi[,3]=NULL
+cutcanadacpi=as.xts(cutcanadacpi)
+names(cutcanadacpi)=c("Date","Rate")
+
+
+
+
+
+
